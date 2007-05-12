cscorley/jedit-mirror

commit 4ecaad67d268e3bfbabf1f3b6c42435b146e9197

Establishes an interface EncodingDetector and makes it a new service in ServiceManager.
Previously provided autodetections (BOM and XMP-PI) are still provided
as builtin services. This offers a way to add new encoding
autodetections like Python, HTML, emacs, ... as a part of a plugin.

This change replaces r8853: option for encoding autodetection from
buffer local property syntax. This autodetection is also provided as a
named service "buffer-local-property". Adding it to new option
"encodingDetectors" in [General] option pane enables this autodetection.

Originally posted as SF.net patch #1698783.


git-svn-id: svn://svn.code.sf.net/p/jedit/svn/jEdit/trunk@9454 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/io/AutoDetection.java
@@ -1,0 +1,162 @@
+/*
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2006 Kazutoshi Satoda
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.io;
+
+//{{{ Imports
+import java.io.InputStream;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.gjt.sp.jedit.jEdit;
+import org.gjt.sp.jedit.ServiceManager;
+import org.gjt.sp.jedit.bufferio.BufferIORequest;
+import org.gjt.sp.jedit.MiscUtilities;
+import org.gjt.sp.util.Log;
+//}}}
+
+/**
+ * Some functions for auto detection of I/O stream properties.
+ */
+public class AutoDetection
+{
+	//{{{ getMarkedStream() method
+	/**
+	 * Returns a marked, rewindable stream.
+	 * Calling reset() method rewinds the stream to its beginning.
+	 * But reset() can fail if too long bytes were read.
+	 */
+	public static BufferedInputStream getMarkedStream(InputStream in)
+	{
+		int bufferSize = BufferIORequest.getByteIOBufferSize();
+		BufferedInputStream markable
+			= new BufferedInputStream(in, bufferSize);
+		assert(markable.markSupported());
+		markable.mark(bufferSize);
+		return markable;
+	} //}}}
+
+	//{{{ isGzipped() method
+	/**
+	 * Returns wheather the stream is gzipped.
+	 * This method reads a few bytes from the sample. So a caller
+	 * must take care of mark() to reuse the contents. Wraping the
+	 * stream by getMarkedStream() is suitable.
+	 */
+	public static boolean isGzipped(InputStream sample)
+		throws IOException
+	{
+		final byte[] GZIP_MAGIC = { (byte)0x1f, (byte)0x8b };
+		return sample.read() == GZIP_MAGIC[0]
+			&& sample.read() == GZIP_MAGIC[1];
+	} //}}}
+
+	//{{{ getEncodingDetectors() method
+	/**
+	 * Returns the user configured ordered list of encoding detectors.
+	 * This method reads property "encodingDetectors".
+	 */
+	public static List<EncodingDetector> getEncodingDetectors()
+	{
+		List<EncodingDetector> detectors
+			= new ArrayList<EncodingDetector>();
+		String propName = "encodingDetectors";
+		String selectedDetectors
+			= jEdit.getProperty(propName, "BOM XML-PI");
+		if (selectedDetectors != null
+			&& selectedDetectors.length() > 0)
+		{
+			for (String name: selectedDetectors.split("\\s+"))
+			{
+				EncodingDetector service
+					= getEncodingDetectorService(name);
+				if (service != null)
+				{
+					detectors.add(service);
+				}
+				else
+				{
+					Log.log(Log.ERROR, AutoDetection.class
+						, "getEncodingDetectors():"
+							+ " No EncodingDetector for the name"
+							+ " \"" + name + "\"");
+				}
+			}
+		}
+		return detectors;
+	} //}}}
+
+	//{{{ getDetectedEncoding() method
+	/**
+	 * Returns an auto detected encoding from content of markedStream.
+	 * This method assumes that markedStream is wrapped by
+	 * getMarkedStream() method.
+	 */
+	public static String getDetectedEncoding(BufferedInputStream markedStream)
+		throws IOException
+	{
+		List<EncodingDetector> detectors = getEncodingDetectors();
+		for (EncodingDetector detector: detectors)
+		{
+			// FIXME: Here the method reset() can fail if the
+			// previous detector read more than buffer size of
+			// markedStream.
+			markedStream.reset();
+			// Wrap once more so that calling mark()
+			// or reset() in detectEncoding() don't
+			// alter the mark position of markedStream.
+			String detected = detector.detectEncoding(
+				new BufferedInputStream(markedStream));
+			if (detected != null)
+			{
+				return detected;
+			}
+		}
+		return null;
+	} //}}}
+
+	//{{{ Private members
+	/**
+	 * Returns a service of EncodingDetector for name.
+	 */
+	private static EncodingDetector getEncodingDetectorService(String name)
+	{
+		String serviceClass = "org.gjt.sp.jedit.io.EncodingDetector";
+		Object service = ServiceManager.getService(serviceClass, name);
+		if (service != null && service instanceof EncodingDetector)
+		{
+			return (EncodingDetector)service;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	//}}}
+}

org/gjt/sp/jedit/MiscUtilities.java
@@ -30,7 +30,6 @@ import javax.swing.JMenuItem;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -47,7 +46,6 @@ import org.gjt.sp.util.StandardUtilities;
 import org.gjt.sp.util.IOUtilities;
 import org.gjt.sp.util.XMLUtilities;
 import org.gjt.sp.jedit.menu.EnhancedMenuItem;
-import org.gjt.sp.jedit.bufferio.BufferIORequest;
 //}}}
 
 /**
@@ -759,134 +757,47 @@ public class MiscUtilities
 	 */
 	public static Reader autodetect(InputStream in, Buffer buffer) throws IOException
 	{
-		in = new BufferedInputStream(in,
-				BufferIORequest.getByteIOBufferSize());
-
 		String encoding;
 		if (buffer == null)
 			encoding = System.getProperty("file.encoding");
 		else
 			encoding = buffer.getStringProperty(Buffer.ENCODING);
+		boolean gzipped = false;
 
-		if(buffer == null || buffer.getBooleanProperty(Buffer.ENCODING_AUTODETECT))
+		BufferedInputStream markedStream
+			= AutoDetection.getMarkedStream(in);
+		if (buffer == null || buffer.getBooleanProperty(Buffer.ENCODING_AUTODETECT))
 		{
-			in.mark(BufferIORequest.XML_PI_LENGTH);
-
-			if(in.read() == BufferIORequest.GZIP_MAGIC_1
-				&& in.read() == BufferIORequest.GZIP_MAGIC_2)
+			gzipped = AutoDetection.isGzipped(markedStream);
+			if (gzipped)
 			{
-				in.reset();
 				Log.log(Log.DEBUG, MiscUtilities.class, "Stream is Gzipped");
-				in = new GZIPInputStream(in);
-				if (buffer != null)
-					buffer.setBooleanProperty(Buffer.GZIPPED,true);
-				// auto-detect encoding within the gzip stream.
-				return autodetect(in, buffer);
+				markedStream.reset();
+				markedStream = AutoDetection.getMarkedStream(
+					new GZIPInputStream(markedStream));
 			}
 
-			in.reset();
-			String detectedByBOM = EncodingWithBOM.detectEncoding(in);
-			if (detectedByBOM != null)
+			markedStream.reset();
+			String detected = AutoDetection.getDetectedEncoding(markedStream);
+			if (detected != null)
 			{
-				encoding = detectedByBOM;
+				encoding = detected;
 			}
-			else
-			{
-				in.reset();
-				byte[] _xmlPI = new byte[BufferIORequest.XML_PI_LENGTH];
-				int offset = 0;
-				int count;
-				while((count = in.read(_xmlPI,offset,
-					BufferIORequest.XML_PI_LENGTH - offset)) != -1)
-				{
-					offset += count;
-					if(offset == BufferIORequest.XML_PI_LENGTH)
-						break;
-				}
-
-				String xmlEncoding = getXMLEncoding(new String(
-					_xmlPI,0,offset,"ASCII"));
-
-				if (xmlEncoding == null)
-				{
-
-					if (Options.X_AUTODETECT)
-					{
-						in.reset();
-						String coding = xAutodetect(in);
-						if (coding != null)
-							encoding = coding;
-					}
-				}
-				else
-				{
-					encoding = xmlEncoding;
-				}
-			}
-
-			in.reset();
 		}
 
 		Log.log(Log.DEBUG, MiscUtilities.class, "Stream encoding detected is " + encoding);
-		Reader result = EncodingServer.getTextReader(in, encoding);
+		markedStream.reset();
+		Reader result = EncodingServer.getTextReader(markedStream, encoding);
 		if (buffer != null)
 		{
-			// Store the successfull encoding.
+			// Store the successfull properties.
+			if (gzipped)
+			{
+				buffer.setBooleanProperty(Buffer.GZIPPED,true);
+			}
 			buffer.setProperty(Buffer.ENCODING, encoding);
 		}
 		return result;
-	} //}}}
-
-	//{{{ xAutodetect() method
-	private static String xAutodetect(InputStream in) throws IOException
-	{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		int i = 0;
-		while (i < 10)
-		{
-			i++;
-			String line = reader.readLine();
-			if (line == null)
-				return null;
-			int pos = line.indexOf(":encoding=");
-			if (pos != -1)
-			{
-				int p2 = line.indexOf(':', pos + 10);
-				String encoding = line.substring(pos + 10, p2);
-				return encoding;
-			}
-		}
-		return null;
-	} //}}}
-
-	//{{{ getXMLEncoding() method
-	/**
-	 * Extract XML encoding name from PI.
-	 */
-	private static String getXMLEncoding(String xmlPI)
-	{
-		if(!xmlPI.startsWith("<?xml"))
-			return null;
-
-		int index = xmlPI.indexOf("encoding=");
-		if(index == -1 || index + 9 == xmlPI.length())
-			return null;
-
-		char ch = xmlPI.charAt(index + 9);
-		int endIndex = xmlPI.indexOf(ch,index + 10);
-		if(endIndex == -1)
-			return null;
-
-		String encoding = xmlPI.substring(index + 10,endIndex);
-
-		if(Charset.isSupported(encoding))
-			return encoding;
-		else
-		{
-			Log.log(Log.WARNING,MiscUtilities.class,"XML PI specifies "
-				+ "unsupported encoding: " + encoding);
-			return null;
-		}
 	} //}}}
 
 	//{{{ closeQuietly() method

org/gjt/sp/jedit/Options.java
@@ -30,10 +30,4 @@ package org.gjt.sp.jedit;
 public class Options
 {
 	public static boolean SIMPLIFIED_KEY_HANDLING;
-
-	/**
-	 * Activate this to activate autodetection of the buffer.
-	 * This is an experimental feature and can fail
-	 */
-	public static boolean X_AUTODETECT;
 }

org/gjt/sp/jedit/ServiceManager.java
@@ -68,6 +68,7 @@ import org.gjt.sp.util.XMLUtilities;
  * <li>{@link org.gjt.sp.jedit.buffer.FoldHandler}</li>
  * <li>{@link org.gjt.sp.jedit.io.VFS}</li>
  * <li>{@link org.gjt.sp.jedit.io.Encoding}</li>
+ * <li>{@link org.gjt.sp.jedit.io.EncodingDetector}</li>
  * </ul>
  *
  * Plugins may provide more.<p>

org/gjt/sp/jedit/bufferio/BufferIORequest.java
@@ -56,18 +56,6 @@ public abstract class BufferIORequest extends WorkRequest
 	//{{{ Constants
 
 	/**
-	 * Magic numbers used for auto-detecting GZIP files.
-	 */
-	public static final int GZIP_MAGIC_1 = 0x1f;
-	public static final int GZIP_MAGIC_2 = 0x8b;
-
-	/**
-	 * Length of longest XML PI used for encoding detection.<p>
-	 * &lt;?xml version="1.0" encoding="................"?&gt;
-	 */
-	public static final int XML_PI_LENGTH = 50;
-
-	/**
 	 * Size of I/O buffers.
 	 */
 	public static final int IOBUFSIZE = 32768;
@@ -92,6 +80,9 @@ public abstract class BufferIORequest extends WorkRequest
 	@Deprecated public static final int UTF8_MAGIC_3 = 0xbf;
 	@Deprecated public static final int UNICODE_MAGIC_1 = 0xfe;
 	@Deprecated public static final int UNICODE_MAGIC_2 = 0xff;
+	@Deprecated public static final int XML_PI_LENGTH = 50;
+	@Deprecated public static final int GZIP_MAGIC_1 = 0x1f;
+	@Deprecated public static final int GZIP_MAGIC_2 = 0x8b;
 
 	//}}}
 
@@ -408,4 +399,5 @@ public abstract class BufferIORequest extends WorkRequest
 	//}}}
 
 }
+
 

org/gjt/sp/jedit/io/BufferLocalEncodingDetector.java
@@ -1,0 +1,62 @@
+/*
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2007 Kazutoshi Satoda
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.io;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * An encoding detector which finds buffer-local-property syntax.
+ *
+ * This reads the sample in the system default encoding for first 10
+ * lines and look for ":encoding=..." syntax. This can fail if the
+ * stream cannot be read in the system default encoding or
+ * ":encoding=..." is not placed at near the top of the stream.
+ *
+ * @since 4.3pre10
+ * @author Kazutoshi Satoda
+ */
+public class BufferLocalEncodingDetector implements EncodingDetector
+{
+	public String detectEncoding(InputStream sample) throws IOException
+	{
+		BufferedReader reader
+			= new BufferedReader(new InputStreamReader(sample));
+		int i = 0;
+		while (i < 10)
+		{
+			i++;
+			String line = reader.readLine();
+			if (line == null)
+				return null;
+			int pos = line.indexOf(":encoding=");
+			if (pos != -1)
+			{
+				int p2 = line.indexOf(':', pos + 10);
+				String encoding = line.substring(pos + 10, p2);
+				return encoding;
+			}
+		}
+		return null;
+	}
+}

org/gjt/sp/jedit/io/EncodingDetector.java
@@ -1,0 +1,40 @@
+/*
+ * :tabSize=8:indentSize=8:noTabs=false:
+ * :folding=explicit:collapseFolds=1:
+ *
+ * Copyright (C) 2007 Kazutoshi Satoda
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.io;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * An interface to detect a reasonable encoding from some bytes at the
+ * beginning of a file.
+ *
+ * @since 4.3pre10
+ * @author Kazutoshi Satoda
+ */
+public interface EncodingDetector
+{
+	/**
+	 * Returns the name of a detected encoding for the bytes in sample.
+	 * Returns null if this instance could not detect reasonable one.
+	 */
+	public String detectEncoding(InputStream sample) throws IOException;
+}

org/gjt/sp/jedit/io/EncodingWithBOM.java
@@ -72,61 +72,64 @@ public class EncodingWithBOM implements Encoding
 	}
 	//}}}
 
-	//{{{ detectEncoding() method
-	public static String detectEncoding(InputStream sample) throws IOException
+	//{{{ class Detector
+	public static class Detector implements EncodingDetector
 	{
-		byte[] mark = new byte[4];
-		int count = sample.read(mark);
-
-		byte low = (byte)(BOM16 & 0xff);
-		byte high = (byte)((BOM16 >> 8) & 0xff);
-		if (count >= 4)
+		public String detectEncoding(InputStream sample) throws IOException
 		{
-			if (mark[0] == low && mark[1] == high
-				&& mark[2] == 0x00 && mark[3] == 0x00)
+			byte[] mark = new byte[4];
+			int count = sample.read(mark);
+	
+			byte low = (byte)(BOM16 & 0xff);
+			byte high = (byte)((BOM16 >> 8) & 0xff);
+			if (count >= 4)
 			{
-				return "X-UTF-32LE-BOM";
-			}
-			else if (mark[0] == 0x00 && mark[1] == 0x00
-				&& mark[2] == high && mark[3] == low)
-			{
-				return "X-UTF-32BE-BOM";
-			}
-		}
-		if (count >= 2)
-		{
-			if (mark[0] == low && mark[1] == high)
-			{
-				return "x-UTF-16LE-BOM";
-			}
-			else if (mark[0] == high && mark[1] == low)
-			{
-				// "x-UTF-16BE-BOM" does not available.
-				// But an encoder for "UTF-16" actually uses
-				// big endian with corresponding BOM. It just
-				// works as "UTF-16BE with BOM".
-				return "UTF-16";
-			}
-		}
-
-		if (count >= UTF8BOM.length)
-		{
-			int i = 0;
-			while (i < UTF8BOM.length)
-			{
-				if (mark[i] != UTF8BOM[i])
+				if (mark[0] == low && mark[1] == high
+					&& mark[2] == 0x00 && mark[3] == 0x00)
 				{
-					break;
+					return "X-UTF-32LE-BOM";
 				}
-				++i;
+				else if (mark[0] == 0x00 && mark[1] == 0x00
+					&& mark[2] == high && mark[3] == low)
+				{
+					return "X-UTF-32BE-BOM";
+				}
 			}
-			if (i == UTF8BOM.length)
+			if (count >= 2)
 			{
-				return "UTF-8Y";
+				if (mark[0] == low && mark[1] == high)
+				{
+					return "x-UTF-16LE-BOM";
+				}
+				else if (mark[0] == high && mark[1] == low)
+				{
+					// "x-UTF-16BE-BOM" does not available.
+					// But an encoder for "UTF-16" actually uses
+					// big endian with corresponding BOM. It just
+					// works as "UTF-16BE with BOM".
+					return "UTF-16";
+				}
 			}
+	
+			if (count >= UTF8BOM.length)
+			{
+				int i = 0;
+				while (i < UTF8BOM.length)
+				{
+					if (mark[i] != UTF8BOM[i])
+					{
+						break;
+					}
+					++i;
+				}
+				if (i == UTF8BOM.length)
+				{
+					return "UTF-8Y";
+				}
+			}
+	
+			return null;
 		}
-
-		return null;
 	} //}}}
 
 	//{{{ Private members

commit 00be03c014cb815c506da9bad44e718e1bbc6939

Fixed broken gzip detection since r9454.
git-svn-id: svn://svn.code.sf.net/p/jedit/svn/jEdit/trunk@9531 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/io/AutoDetection.java
@@ -73,9 +73,10 @@ public class AutoDetection
 	public static boolean isGzipped(InputStream sample)
 		throws IOException
 	{
-		final byte[] GZIP_MAGIC = { (byte)0x1f, (byte)0x8b };
-		return sample.read() == GZIP_MAGIC[0]
-			&& sample.read() == GZIP_MAGIC[1];
+		int magic1 = GZIPInputStream.GZIP_MAGIC & 0xff;
+		int magic2 = (GZIPInputStream.GZIP_MAGIC >> 8) & 0xff;
+		return sample.read() == magic1
+			&& sample.read() == magic2;
 	} //}}}
 
 	//{{{ getEncodingDetectors() method